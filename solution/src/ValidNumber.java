4074a3a1d7755871897ebee8d478ff3f44c18052
AaronZhou-WHU <zb89china@163.com>
2018-08-03 23:58:36 +0800

    Valid Number update

--- solution/src/ValidNumber.java
+++ solution/src/ValidNumber.java
@@ -5,21 +5,29 @@
  */
 public class ValidNumber {
     public static void main(String[] args) {
-        String s = "-1.0a";
+        String s = ".1";
         System.out.println(isNumber(s));
     }
     public static boolean isNumber(String s) {
-        if (null==s||s.length()==0)
+        if (null==s||s.trim().length()==0)
             return false;
         char[] chars = s.trim().toCharArray();
         for (int i = 0; i < chars.length; i++) {
             if(i==0){
-                if ((chars[i]<'0'||chars[i]>'9')&&chars[i]!='-'){
+                if ((chars[i]<'0'||chars[i]>'9')&&chars[i]!='-'&&chars[i]!='.'){
                     return false;
+                }else if (chars[i]=='-'||chars[i]=='.'){
+                    if (i==chars.length-1||chars[i]<'0'||chars[i]>'9'){
+                        return false;
+                    }
                 }
             }else {
                 if ((chars[i]<'0'||chars[i]>'9')&&chars[i]!='e'&&chars[i]!='.'){
                     return false;
+                }else if (chars[i]=='-'||chars[i]=='.'){
+                    if (i==chars.length-1||chars[i]<'0'||chars[i]>'9'){
+                        return false;
+                    }
                 }
             }
 
