cffc73a472a38efbe7a400ac347dd3c49610c8be
AaronZhou-WHU <zb89china@163.com>
2018-08-06 12:05:48 +0800

    String to Integer

--- solution/src/StringtoInteger.java
+++ solution/src/StringtoInteger.java
@@ -5,35 +5,44 @@
  */
 public class StringtoInteger {
     public static void main(String[] args) {
-        String s = "-";
+        String s = "+1";
         //String s = "-2147483648";
         System.out.println(myAtoi(s));
     }
+
     public static int myAtoi(String str) {
-        if (null==str||str.trim().length()==0)
+        if (null == str || str.trim().length() == 0)
             return 0;
         str = str.trim();
         StringBuffer res = new StringBuffer();
         char[] chars = str.toCharArray();
         for (int i = 0; i < chars.length; i++) {
-            if (chars[i]=='-'&&i==0){
+            if (chars[i] == '-' && i == 0) {
                 res.append(chars[i]);
-            }else if (chars[i]>='0'&&chars[i]<='9'){
+            } else if (chars[i] == '+' && i == 0) {
+                //res.append(chars[i]);
+            } else if (chars[i] >= '0' && chars[i] <= '9') {
                 res.append(chars[i]);
-            }else {
+            } else {
                 break;
             }
         }
-        if (res.length()==0){
+        if (res.length() == 0) {
             return 0;
-        }else if (res.toString().equals("-")&&res.length()==1){
+        } else if (res.toString().equals("-") && res.length() == 1) {
             return 0;
+        } else if (res.toString().startsWith("-")){
+            try {
+                return Integer.parseInt(res.toString());
+            } catch (Exception e) {
+                int si = Integer.MIN_VALUE;
+                return -si;
+            }
         }else {
             try {
                 return Integer.parseInt(res.toString());
-            }catch (Exception e){
-                int si = Integer.MIN_VALUE;
-                return -si;
+            } catch (Exception e) {
+                return Integer.MAX_VALUE;
             }
         }
     }
