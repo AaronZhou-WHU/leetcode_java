a8c8daace4838efe859dea5a875166c781a2133e
AaronZhou-WHU <zb89china@163.com>
2018-08-17 23:44:15 +0800

    ConstructBinaryTree-- pre&in

---
+++ solution/src/K_SimilarStrings.java
@@ -0,0 +1,9 @@
+package PACKAGE_NAME;
+
+/**
+ * @Author : zhoubin
+ * @Description :
+ * @Date : 18/8/16 09:16
+ */
+public class K_SimilarStrings {
+}
--- solution/src/foroffer/ConstructBinaryTree.java
+++ solution/src/foroffer/ConstructBinaryTree.java
@@ -1,5 +1,7 @@
 package foroffer;
 
+import java.util.Arrays;
+
 /**
  * @Author : zhoubin
  * @Description :
@@ -12,14 +14,28 @@ public class ConstructBinaryTree {
 
     public static TreeNode reConstructBinaryTree(int[] pre, int[] in) {
         TreeNode root = new TreeNode(0);
-        root.val= pre[0];
+        if (pre.length==0){
+            return root;
+        }
+        root.val=pre[0];
         int index = 0;
         for (int i = 0; i < in.length; i++) {
-            if (in[i]==root.val){
+            if (in[i]==pre[0]){
                 index = i;
+                break;
             }
         }
-        //int[] left = in.
+        int[] leftIn = Arrays.copyOfRange(in,0,index);
+        int[] rightIn = Arrays.copyOfRange(in,index+1,in.length);
+        int[] leftPre = Arrays.copyOfRange(pre,1,index+1);
+        int[] rightPre = Arrays.copyOfRange(pre,index+1,pre.length);
+        if (leftPre.length>0){
+            root.left=reConstructBinaryTree(leftPre,leftIn);
+        }
+        if (rightPre.length>0){
+            root.right=reConstructBinaryTree(rightPre,rightIn);
+        }
+
         return root;
     }
 }
