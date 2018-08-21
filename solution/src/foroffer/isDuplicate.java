00654fc3e741acaa67f3c5d14ca44a434046dbfe
AaronZhou-WHU <zb89china@163.com>
2018-08-22 01:09:19 +0800

    Duplicate

--- solution/src/foroffer/isDuplicate.java
+++ solution/src/foroffer/isDuplicate.java
@@ -6,4 +6,29 @@ package foroffer;
  * @Date : 18/8/22 00:35
  */
 public class isDuplicate {
+    public static void main(String[] args) {
+        int[] arr = {2,1,3,0,4};
+        int[] dd =  {2,4,3,1,4};
+        System.out.println(duplicate(arr,5,dd));
+    }
+    public static boolean duplicate(int numbers[],int length,int [] duplication) {
+        if(numbers==null||numbers.length==0||length==0)
+            return false;
+
+        int j =0;
+        for (int i = 0; i < length; i++) {
+            if (numbers[i]==i)
+                continue;
+            else if (numbers[i]!=i){
+                int tmp = numbers[i];
+                numbers[i] = numbers[tmp];
+                numbers[tmp] = tmp;
+            }
+            if (numbers[i]!=i&&numbers[i]==numbers[numbers[i]]){
+                duplication[0] = numbers[i];
+                return true;
+            }
+        }
+        return false;
+    }
 }
