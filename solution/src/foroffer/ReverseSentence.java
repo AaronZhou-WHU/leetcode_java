e437456a6bc8000a845b8e50defb53e9bd3cc71c
AaronZhou-WHU <zb89china@163.com>
2018-09-14 01:12:10 +0800

    ReverseSentence

--- solution/src/foroffer/ReverseSentence.java
+++ solution/src/foroffer/ReverseSentence.java
@@ -6,4 +6,17 @@ package foroffer;
  * @Date : 18/9/14 01:02
  */
 public class ReverseSentence {
+    public String reverseSentence(String str) {
+        if (null==str||str.length()==0)
+            return str;
+        String[] strings = str.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = strings.length-1; i >=0; i--) {
+            sb.append(strings[i]);
+            if (i != 0) {
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
+    }
 }
